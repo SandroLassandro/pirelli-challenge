cc2e7ff5012e3314447f063d37d160dae47d8d93
Sandro <sandro.lassandro@gmail.com>
2024-03-03 16:25:37 +0100

    Theme-example SVG + generic css improvements

--- src/app/theme-picker/theme-picker.component.ts
+++ src/app/theme-picker/theme-picker.component.ts
@@ -3,11 +3,6 @@ import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
 /**
- * Theme example SVG
- */
-const ThemeExample = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 80 80" fit="" preserveAspectRatio="xMidYMid meet" focusable="false"><defs><path d="M77.87 0C79.05 0 80 .95 80 2.13v75.74c0 1.17-.95 2.13-2.13 2.13H2.13C.96 80 0 79.04 0 77.87V2.13C0 .95.96 0 2.13 0h75.74z" id="a"></path><path d="M54 40c3.32 0 6 2.69 6 6 0 1.2 0-1.2 0 0 0 3.31-2.68 6-6 6H26c-3.31 0-6-2.69-6-6 0-1.2 0 1.2 0 0 0-3.31 2.69-6 6-6h28z" id="b"></path><path d="M0 0h80v17.24H0V0z" id="c"></path></defs><use xlink:href="#a" fill="#fff" class="docs-theme-icon-background"></use><use xlink:href="#b" fill="#ff4081" class="docs-theme-icon-button"></use><use xlink:href="#c" fill="#3f51b5" class="docs-theme-icon-toolbar"></use></svg>';
-
-/**
  * Theme
  */
 interface Theme {
@@ -40,12 +35,16 @@ export class ThemePickerComponent implements OnInit {
     displayName: 'Purple & Green'
   }];
   /**
+   * Theme example SVG path
+   */
+  private readonly _themeExampleSVGPath = 'assets/icons/theme-example.svg'
+  /**
    * Current theme name
    */
   public currentTheme!: string;
 
   constructor(_iconRegistry: MatIconRegistry, _sanitizer: DomSanitizer) {
-    _iconRegistry.addSvgIconLiteral('theme-example', _sanitizer.bypassSecurityTrustHtml(ThemeExample));
+    _iconRegistry.addSvgIcon('theme-example', _sanitizer.bypassSecurityTrustResourceUrl(this._themeExampleSVGPath));
   }
 
   public ngOnInit(): void {
